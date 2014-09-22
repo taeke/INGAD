0893cacbb3b12cbad5c990603346c284d00f9e29
taeke <taekevdv@gmail.com>
2014-09-22 12:28:11 +0200

    StyleCop en GetHtmlTest toegevoegd

--- GetHtmlTest/Properties/AssemblyInfo.cs
+++ GetHtmlTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("INGAD")]
+[assembly: AssemblyTitle("GetHtmlTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("INGAD")]
+[assembly: AssemblyProduct("GetHtmlTest")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c1acb693-33d0-45d2-8835-986e3ee73ae2")]
+[assembly: Guid("841c5569-7df2-43c0-bdf2-be9914fa8587")]
 
 // Version information for an assembly consists of the following four values:
 //
